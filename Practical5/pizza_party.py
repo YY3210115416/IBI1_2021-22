38267a4caa7d2fcff903bd144277388d3925459b
YY3210115416 <yangy.21@intl.zju.edu.cn>
2022-05-15 16:33:33 +0800

    explain what are n and p

--- Practical5/pizza_party.py
+++ Practical5/pizza_party.py
@@ -2,6 +2,7 @@
 p = 1
 n = 0
 #When there are less than 64 pieces of cake, we need to cut one more time and calculate the number of pieces.
+#p stands for the number of pieces, n stands for the number of cuts.
 while p < 64:
  n = n + 1
  p = (n*n+n+2)/2
